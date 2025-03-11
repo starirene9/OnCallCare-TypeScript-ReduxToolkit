07d94d0a46810622fda9a0794c6c98c14b3b39a1
starirene9 <>
2025-03-11 14:27:21 +0900

    useFetch added

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = () => {
-  return <div>useFetch</div>;
+type FetchState<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
 };
+
+function useFetch<T>(
+  url: string,
+  options?: RequestInit,
+  dependencies: any[] = []
+): FetchState<T> {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    setLoading(true);
+    setError(null);
+
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        if (isMounted) {
+          setData(jsonData);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, [url, ...dependencies]);
+  return { data, loading, error };
+}
 
 export default useFetch;
