10a24e51eab79f8e3838dfc46f969951b951b601
starirene9 <>
2025-03-12 13:35:56 +0900

    Version added on Footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,11 @@
+import packageJson from "../../../package.json";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white p-3 h-12 w-full flex items-center justify-end pr-5  fixed bottom-0">
-      <p className="font-normal text-sm">© 2025 OnCallCare by Bitna Gu</p>
+      <p className="font-normal text-sm">
+        © 2025 OnCallCare by Bitna Gu | v{packageJson.version}
+      </p>
     </footer>
   );
 };
